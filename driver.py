d16ee48bc0c2429b3980a5cf90ce5ce33520ca91
sai-k02 <srilokh.karuturi@gmail.com>
2022-10-05 21:38:26 -0500

    finish

--- driver.py
+++ driver.py
@@ -4,27 +4,27 @@ import datetime
 import sys
 import time
 
+# DEFINE LIST OF AVAILABLE COMMANDS TO DRIVE PROGRAM
+availableCommands = set(
+    ['password', 'encrypt', 'decrypt', 'history', 'quit'])
 
-def getCurrentTime() -> str:
-    currentTime = datetime.datetime.now()
-    currentTime = "%s-%s-%s %s:%s" % (currentTime.year, currentTime.month,
-                                      currentTime.day, currentTime.hour, currentTime.minute)
+# HISTORY DICT
+history = {}
+history['password'] = []
+history['encrypt'] = []
+history['decrypt'] = []
 
-    return currentTime
 
+def handleHistory(command: str):
+    print("Previous " + command + "s: ")
 
-def getLogString(action: str, message: str) -> str:
-
-    return getCurrentTime() + ' ['+action+'] ' + message + '\n'
+    for element in history[command]:
+        print("\t- "+element)
 
 
 async def main():
-    # KEEP TRACK OF ENCRYPTED AND UNENCRYPTED STRINGS
-    toEncryptHistory = ()
-    encryptedHistory = ()
-
     # PRINT USAGE
-    usage = "usage: \n\tpassword <message> " + "\n\tencrypt <message> " + "\n\tdecrypt <message> " + \
+    usage = "Python program which will allow encryption/decryption with Cesar Cipher using subprocesses\nusage: \n\tpassword <message> " + "\n\tencrypt <message> " + "\n\tdecrypt <message> " + \
         "\n\thistory <password | encrypt | decrypt>" + "\n\tquit"
 
     print(usage)
@@ -39,18 +39,16 @@ async def main():
         encrypt = await asyncio.create_subprocess_exec(
             "python3", "encrypt.py", sys.argv[1], stdin=PIPE, stdout=PIPE)
 
-        logger.stdin.write(bytes('STOP\n Driver Started\n.', 'utf-8'))
+        logger.stdin.write(bytes('START\n Driver Started.\n', 'utf-8'))
 
         # DEFINE USER INPUT
         userInput = ''
 
         # DEFINE LIST OF COMMANDS
-        availableCommands = set(
-            ['password', 'encrypt', 'decrypt', 'history', 'quit'])
 
         while (userInput != 'quit'):
             # GET USER INPUT
-            print("Input: ", end='')
+            print("\nInput: ", end='')
             userInput = input()
             userInputList = userInput.split(' ')
             if (len(userInputList) == 2 and userInput != 'quit'):
@@ -59,29 +57,68 @@ async def main():
                 message = userInputList[1]
 
                 if (command == 'password'):
-                    pass
+                    logger.stdin.write(
+                        bytes(command.upper()+'\n'+message+'\n', 'utf-8'))
+                    encrypt.stdin.write(
+                        bytes('PASSKEY\n'+message+'\n', 'utf-8'))
+                    result = bytes.decode(await encrypt.stdout.readline()).rstrip()
+                    if result != 'ERROR':
+                        history[command].append(message)
+                    message = bytes.decode(await encrypt.stdout.readline()).rstrip()
+                    logger.stdin.write(
+                        bytes(result+'\n'+message+'\n', 'utf-8'))
+
                 if (command == 'encrypt'):
-                    pass
+                    logger.stdin.write(
+                        bytes(command.upper()+'\n'+message+'\n', 'utf-8'))
+                    encrypt.stdin.write(
+                        bytes('ENCRYPT\n'+message+'\n', 'utf-8'))
+                    result = bytes.decode(await encrypt.stdout.readline()).rstrip()
+                    if result != 'ERROR':
+                        history[command].append(message)
+                    message = bytes.decode(await encrypt.stdout.readline()).rstrip()
+
+                    logger.stdin.write(
+                        bytes(result+'\n'+message+'\n', 'utf-8'))
+
+                    logger.stdin.write(
+                        bytes(result+'\n'+message+'\n', 'utf-8'))
                 if (command == 'decrypt'):
-                    pass
+                    logger.stdin.write(
+                        bytes(command.upper()+'\n'+message+'\n', 'utf-8'))
+                    encrypt.stdin.write(
+                        bytes('DECRYPT\n'+message+'\n', 'utf-8'))
+                    result = bytes.decode(await encrypt.stdout.readline()).rstrip()
+                    if result != 'ERROR':
+                        history[command].append(message)
+                    message = bytes.decode(await encrypt.stdout.readline()).rstrip()
+
+                    logger.stdin.write(
+                        bytes(result+'\n'+message+'\n', 'utf-8'))
+
+                    logger.stdin.write(
+                        bytes(result+'\n'+message+'\n', 'utf-8'))
                 if (command == 'history'):
-                    pass
+                    if (message in availableCommands):
+                        logger.stdin.write(
+                            bytes(command.upper()+'\n'+message+'\n', 'utf-8'))
+                        print("Previous " + message + "s: ")
+                        for element in history[message]:
+                            print("\t- "+element)
+                    else:
+                        print("usage: <command> <message>")
 
-                logger.stdin.write(bytes(command+'\n'+message+'\n', 'utf-8'))
-                encrypt.stdin.write(bytes(command+'\n'+message+'\n', 'utf-8'))
-
-                result = bytes.decode(await encrypt.stdout.readline()).rstrip()
-                message = bytes.decode(await encrypt.stdout.readline()).rstrip()
-
-                logger.stdin.write(bytes(result+'\n'+message+'\n', 'utf-8'))
-
-                print("Output: %s %s" % (result, message))
+                if (command != 'history'):
+                    print("Output: %s %s" % (result, message))
+            elif userInputList[0] == 'quit':
+                logger.stdin.write(bytes('STOP\n Driver STOP.\n', 'utf-8'))
+                logger.stdin.write(bytes('QUIT\n', 'utf-8'))
+                encrypt.stdin.write(bytes('QUIT\n', 'utf-8'))
             else:
-                pass
-        logger.stdin.write(bytes('STOP\n Driver Stopped\n.', 'utf-8'))
-        logger.stdin.write(bytes('QUIT\n', 'utf-8'))
-        encrypt.stdin.write(bytes('QUIT\n', 'utf-8'))
-        logFile.write(getLogString('STOP', 'Driver Stopped.'))
+                print("usage: <command> <message>")
+
+        # logger.kill()
+        # encrypt.kill()
 
 if (len(sys.argv) != 2):
     print("usage: driver.py <output.txt>")
